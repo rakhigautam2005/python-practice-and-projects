86883087b2031cb4cdfeb3fe0e88db51f8af0e03
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:18:02 +0530

    Delete chapter7 directory

--- chapter7/03_loops.py
+++
@@ -1,6 +0,0 @@
-#program to print  1 to 50 using a while loop
-i = 0
-while i<5:#print "harry" -5 times!
-    print("harry")
-    i = i+1 # i = i+1
-    
--- chapter7/04_loops.py
+++
@@ -1,7 +0,0 @@
-#write a program to print the content of list using while loops
-l = [1, "harry",False,"that","rohan","shubhum","shubhi"]
-
-i = 0
-while(i<len(l)):
-    print(l[i])
-    i +=1

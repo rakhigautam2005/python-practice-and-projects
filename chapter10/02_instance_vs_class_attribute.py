8eb520fbccae1d9f221f00ab6743981531de39aa
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:12:54 +0530

    Delete chapter10 directory

--- chapter10/02_instance_vs_class_attribute.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    name  = "Rakhi"
-    language = "py"#this is the class attribute.
-    salary = "120000"
-
-rakhi = Employee()
-#rakhi.language = "js"#this is an instance attribute and it will take preference over class attributes aduring assignment and retrieval 
-print(rakhi.language,rakhi.salary)
-

528865473d56be67986bcf35ea485bda0defb9e6
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:15:33 +0530

    Delete chapter3 directory

--- chapter3/04_escape_sequence.py
+++
@@ -1,4 +0,0 @@
-#a = "Harry is a good boy\nbut not a \nbad boy"
-a = "Harry is a good boy\nbut not a bad \"boy\""#same for single quote also
-print(a)
-#if we don't want python to treat \as escape charcter then always use r at the starting of the sentence

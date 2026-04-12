3052e8afede2f18030631852af1e77b624935a54
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:11:34 +0530

    Delete chaper4 ps directory

--- chaper4 ps/03_problem.py
+++
@@ -1,4 +0,0 @@
-# Check that a tuple type cannot be changed in python.
-a = (34, 234, "jar")
-
-a[2] = "car"
--- chaper4 ps/04_problem.py
+++
@@ -1,5 +0,0 @@
-#  Write a program to sum a list with 4 numbers.
-l = [3, 3, 5, 1]
-
-
-print(sum(l))

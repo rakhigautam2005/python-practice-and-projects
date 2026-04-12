df092b29fdf1973c769269524157adf9f0c3ae7b
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:16:06 +0530

    Delete chapter4 directory

--- chapter4/01_tuple.py
+++
@@ -1,9 +0,0 @@
-#immutable in nature and can't be changed
-#a = (1 ,4, 6, 7, 8)
-#print(type(a))
-#a=()#empty tuple
-#a = (1,)#if we put , after 1 it will be considered as tuple and without , it will be as integer
-#print(type(a))
-a =(1, 23 , 342, 3423, False, "Rohan", "Shivam" )#can't be changed
-print(a)
-print(type(a))

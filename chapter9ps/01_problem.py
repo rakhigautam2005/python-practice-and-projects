725aca75a5b35afef7bffc8b52b020981b79168b
rakhigautam2005 <2k23.ece2311671@gmail.com>
2026-04-12 17:19:34 +0530

    Delete chapter9ps directory

--- chapter9ps/01_problem.py
+++
@@ -1,8 +0,0 @@
-#wap  to read the text from given file 'poems.txt' and find out where it contains the word 'twinkle'.
-f = open("poem.txt")
-content = f.read()
-if("twinkle" in content):
-    print("twinkle is present in the content")
-else:
-    print("twinkle is not present in the content")
-f.close()
--- chapter9ps/03_problem.py
+++
@@ -1,9 +0,0 @@
-#wap to generate multiplication tables from 2 to 20 and write it to the  different files.Place these files in folder for 13-yera old.
-def generateTable(n):
-    table = ""#to make a string
-    for i in range(1, 11):
-        table += f"{n}x{i} = {n*i}\n"#added to fstring ti make a new  string 
-        f.write(table)
-    with open(f"tables/table_{n}.txt", "w")as f:#and then we will add it to   file by opening a folder
-        for i in range(2, 21):
-            generateTable(i)
